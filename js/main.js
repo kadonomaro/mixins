d7a34cb4a9e6917d156a5a4d5bbd75014c98b089
kadonomaro <denis.afer@gmail.com>
2018-12-11 23:04:35 +0700

    add accordion base

--- js/main.js
+++ js/main.js
@@ -2,7 +2,18 @@ $(document).ready(function(){
     $(".card-slider").slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToScroll: 1,
         dots: true
     });
 });
+
+
+// window.onload = function () {
+//     var accToggle = document.querySelectorAll('.accordion__head');
+
+//     accToggle.forEach(element => {
+//         element.addEventListener('click', function () { 
+//             this.
+//         });
+//     });
+// }
