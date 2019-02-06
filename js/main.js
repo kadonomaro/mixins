d06ddce821cefaadb99e852fffaff2759076ca83
kadonomaro <denis.afer@gmail.com>
2019-02-06 19:43:55 +0700

    parallax

--- js/main.js
+++ js/main.js
@@ -25,11 +25,16 @@ window.onload = function () {
     column.addEventListener('click', function () {
         column.classList.toggle('row__side_size_large');
     });
-
+    console.dir(window);
     let big = document.getElementById('big');
+    console.dir(big);
     window.addEventListener('scroll', function () {
-        
+        console.log("win: ",window.pageYOffset,"big: ", big.offsetTop);
+        big.style.backgroundPosition = `top ${(big.offsetTop - window.pageYOffset) /2}px left 20px, 
+                                        top ${(big.offsetTop - window.pageYOffset) /2.5}px left 60px,
+                                        top ${(big.offsetTop - window.pageYOffset) /3}px left 100px,
+                                        top ${(big.offsetTop - window.pageYOffset) /3.5}px left 140px`;
     });
     
-
+    // mainTitle.style.transform = `translateY(${window.pageYOffset / 2}px)`;
 }
